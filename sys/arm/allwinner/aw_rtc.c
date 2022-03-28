dbbef77d14004214219d59f785329ab4f322ad86
Julien Cassette <julien.cassette@gmail.com>
2022-03-28 21:53:55 +0000

    riscv: support Allwinner D1 clock controllers
    The CCU controls reset and clocks of most peripherals, so this adds
    the definitions needed by the aw_ccung driver to support the D1.
    
    The RTC clock outputs (iosc and osc32k) are needed by the CCU so this
    also adds the D1 compatible data to aw_rtc.
    
    Signed-off-by: Julien Cassette <julien.cassette@gmail.com>

--- sys/arm/allwinner/aw_rtc.c
+++ sys/arm/allwinner/aw_rtc.c
@@ -137,6 +137,7 @@ static struct ofw_compat_data compat_data[] = {
 	{ "allwinner,sun7i-a20-rtc", (uintptr_t) &a20_conf },
 	{ "allwinner,sun6i-a31-rtc", (uintptr_t) &a31_conf },
 	{ "allwinner,sun8i-h3-rtc", (uintptr_t) &h3_conf },
+	{ "allwinner,sun20i-d1-rtc", (uintptr_t) &h3_conf },
 	{ "allwinner,sun50i-h5-rtc", (uintptr_t) &h3_conf },
 	{ "allwinner,sun50i-h6-rtc", (uintptr_t) &h3_conf },
 	{ NULL, 0 }
@@ -255,8 +256,11 @@ aw_rtc_install_clocks(struct aw_rtc_softc *sc, device_t dev) {
 	node = ofw_bus_get_node(dev);
 	nclocks = ofw_bus_string_list_to_array(node, "clock-output-names", &clknames);
 	/* No clocks to export */
-	if (nclocks <= 0)
+	if (nclocks <= 0) {
+		if (bootverbose)
+			device_printf(dev, "No clocks to export\n");
 		return;
+	}
 
 	if (nclocks != 3) {
 		device_printf(dev, "Having only %d clocks instead of 3, aborting\n", nclocks);
